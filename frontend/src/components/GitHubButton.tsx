a7a8b42c5990e74fb75dac004a18c8d06afd81e1
Jonas Franz <info@jonasfranz.software>
2018-06-13 14:04:48 +0200

    Add prettier Fix lint errors Improve lint config
    Signed-off-by: Jonas Franz <info@jonasfranz.software>

--- frontend/src/components/GitHubButton.tsx
+++ frontend/src/components/GitHubButton.tsx
@@ -3,31 +3,37 @@ import { Button, Icon } from "semantic-ui-react";
 import { client } from "../configuration";
 
 export interface IGitHubButtonState {
-    loading: boolean
+  loading: boolean;
 }
 
 export class GitHubButton extends React.Component<{}, IGitHubButtonState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
 
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            loading: false,
-        };
-    }
+  render() {
+    return (
+      <Button
+        icon
+        loading={this.state.loading}
+        disabled={this.state.loading}
+        labelPosition="left"
+        onClick={this.submitToGitHub}
+      >
+        <Icon name="github" />
+        Login with GitHub
+      </Button>
+    );
+  }
 
-    render() {
-        return <Button icon={true} loading={this.state.loading} disabled={this.state.loading} labelPosition='left' onClick={this.submitToGitHub}>
-            <Icon name='github'/>
-            Login with GitHub
-        </Button>
-    }
-
-    submitToGitHub() {
-        this.setState({loading: true})
-        client.getGitHubRedirect()
-        .then(res => {
-            this.setState({loading: false})
-            window.location.href = res.url
-        })
-    }
+  submitToGitHub() {
+    this.setState({ loading: true });
+    client.getGitHubRedirect().then(res => {
+      this.setState({ loading: false });
+      window.location.href = res.url;
+    });
+  }
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { hot } from 'react-hot-loader';
+import { hot } from "react-hot-loader";
 
-import { WelcomePage } from "./components/WelcomePage";
-import { HashRouter as Router, Route, Link} from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { GitHubButton } from "./components/GitHubButton";
+import { WelcomePage } from "./components/WelcomePage";
 
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route exact path="/" component={WelcomePage}/>
-            <Route path="/callback" component={GitHubButton}/>
-        </div>
-    </Router>,
-    document.getElementById("example")
+  <Router>
+    <div>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/callback" component={GitHubButton} />
+    </div>
+  </Router>,
+  document.getElementById("example")
 );
 
-export default hot(module)(WelcomePage)
+export default hot(module)(WelcomePage);
